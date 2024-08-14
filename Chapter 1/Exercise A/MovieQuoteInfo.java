4d49e1e9b95dfc2e086707152936feed85c66d8f
mjsilva098 <michaeljohnsilva5@gmail.com>
2024-08-14 19:27:47 +0800

    First Commit

---
+++ Chapter 1/Exercise A/MovieQuoteInfo.java
@@ -0,0 +1,15 @@
+public class MovieQuoteInfo
+{
+   public static void main(String[] args)
+   {
+      String quote = "When you plan something well, there's no need to rush.";
+      String movie = "Peaky Blinders";
+      String character = "Tommy Shelby";
+      int year = 2013;
+      
+      System.out.println("Quote: " + quote);
+      System.out.println("Movie: " + movie);
+      System.out.println("Character: " + character);
+      System.out.println("Year: " + year);
+   }
+}
