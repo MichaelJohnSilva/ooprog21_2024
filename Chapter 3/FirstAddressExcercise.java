b0962bdb15325a9c2ab04dc7d1d3950ba3a9461d
MIchael John W. Silva <michaeljohnsilva5@gmail.com>
2024-09-16 09:44:20 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,16 @@
+public class FirstAddressExcercise
+{
+   public static void main(String[] args)
+   {
+      System.out.println("First Java application");
+      displayAddress();
+   }
+   public static void displayAddress()
+   {
+      System.out.println("XYZ Company");
+      System.out.println("129 Samsung-ro");
+      System.out.println("Yeongtong-gu, Suwon-si");
+      System.out.println("Gyeonggi-do, South Korea");
+      System.out.println("Company: Samsung Electronics");
+   }
+}
